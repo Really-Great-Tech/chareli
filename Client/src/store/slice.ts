85763371ed7cb772052b6a0a989f8538f18c2a4e
hipihapa <afriyiefrankasumadu@gmail.com>
2025-05-12 17:50:27 +0000

    tailwind configuration fixes, implementing shadcn and react router installation

---
+++ Client/src/store/slice.ts
@@ -0,0 +1,32 @@
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// // Define the type for your slice state
+// interface YourState {
+//   value: number;
+// }
+
+// // Initial state
+// const initialState: YourState = {
+//   value: 0,
+// };
+
+// // Create the slice
+// const slice = createSlice({
+//   name: 'slice',
+//   initialState,
+//   reducers: {
+//     increment: (state) => {
+//       state.value += 1;
+//     },
+//     decrement: (state) => {
+//       state.value -= 1;
+//     },
+//     setValue: (state, action: PayloadAction<number>) => {
+//       state.value = action.payload;
+//     },
+//   },
+// });
+
+// // Export actions and reducer
+// export const { increment, decrement, setValue } = slice.actions;
+// export default slice.reducer;

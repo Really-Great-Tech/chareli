f798e6599406abcd38335cabfbc43221a1107081
Edmond Amofah Boakye <edmondboakye1622@gmail.com>
2025-05-18 14:03:38 +0000

    update to auth

--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -2,14 +2,10 @@ import { useState } from 'react';
 import AllGamesSection from '../../components/single/AllGamesSection'
 import PopularSection from '../../components/single/PopularSection'
 import { WelcomeModal } from '../../components/modals/WelcomeModal';
-import { useAuth } from '../../context/AuthContext';
+
 
 function Home() {
   const [isWelcomeModalOpen, setIsWelcomeModalOpen] = useState(true);
-  const { user } = useAuth()
-
-  console.log("user", user)
-
 
   return (
     <div className='font-boogaloo'>

bffef0be95d0d948b634aaa66506e54ab79a878f
Asumadu Frank Afriyie <afriyiefrankasumadu@gmail.com>
2025-07-30 12:53:01 +0000

    update component positions...

--- Client/src/pages/Admin/Analytics/Analytics.tsx
+++ Client/src/pages/Admin/Analytics/Analytics.tsx
@@ -16,34 +16,6 @@ export default function Analytics() {
   console.log("Age Counts:", { adultsCount, minorsCount, totalRegistered });
   return (
     <div className="space-y-6">
-      {/* bar chart */}
-      <div className="w-full">
-        <Card className="bg-[#F1F5F9] dark:bg-[#121C2D] shadow-none border-none w-full">
-          <div className="justify-between items-center flex p-3">
-            <p className="text-base sm:text-xl lg:text-2xl">Click insights</p>
-          </div>
-          {/* inner card */}
-          <Card className="bg-[#F8FAFC] dark:bg-[#0F1221] shadow-none border-none mx-3 p-4">
-            <div className="flex flex-col space-y-8">
-              <div className="">
-                <div className="justify-start flex items-center gap-4">
-                  <img
-                    src={click}
-                    alt="users"
-                    className="w-10 h-10 dark:text-white"
-                  />
-                  <p className="text-sm sm:text-base lg:text-lg text-[#64748A] dark:text-white">
-                    Total clicks on Sign-up button
-                  </p>
-                </div>
-
-                <HorizontalBarChart />
-              </div>
-            </div>
-          </Card>
-        </Card>
-      </div>
-
       {/* bar chart for user age */}
       {/* <div className="w-full">
         <Card className="bg-[#F1F5F9] dark:bg-[#121C2D] shadow-none border-none w-full">
@@ -101,6 +73,34 @@ export default function Analytics() {
                 </div>
 
                 <DonutChart />
+              </div>
+            </div>
+          </Card>
+        </Card>
+      </div>
+
+      {/* bar chart */}
+      <div className="w-full">
+        <Card className="bg-[#F1F5F9] dark:bg-[#121C2D] shadow-none border-none w-full">
+          <div className="justify-between items-center flex p-3">
+            <p className="text-base sm:text-xl lg:text-2xl">Click insights</p>
+          </div>
+          {/* inner card */}
+          <Card className="bg-[#F8FAFC] dark:bg-[#0F1221] shadow-none border-none mx-3 p-4">
+            <div className="flex flex-col space-y-8">
+              <div className="">
+                <div className="justify-start flex items-center gap-4">
+                  <img
+                    src={click}
+                    alt="users"
+                    className="w-10 h-10 dark:text-white"
+                  />
+                  <p className="text-sm sm:text-base lg:text-lg text-[#64748A] dark:text-white">
+                    Total clicks on Sign-up button
+                  </p>
+                </div>
+
+                <HorizontalBarChart />
               </div>
             </div>
           </Card>
